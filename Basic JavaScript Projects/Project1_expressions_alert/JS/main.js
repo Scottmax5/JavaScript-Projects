2199877cb701851ef103a83534322ae98a71b0d0
Scottmax5 <127995861+Scottmax5@users.noreply.github.com>
2023-04-13 14:29:46 -0700

    Javaproject
    Added my files to JavaScript Project repository.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("Hello, world!");      //Alert that says "Hello, world! when opened"
+
+document.write('Hello, world!');    //Line of text saying Hello, world!
+document.write("<br><br>");         //Line break
+
+var Sent1 = "This is the beginning of the string";      //Variable given to Sent1
+var Sent2 = " This is the beginning of the string";     //Variable given to Sent2
+
+document.write(Sent1 + Sent2);      //Printing Sent1 and Sent2 variables
+document.write("<br><br>");         //Line break
+
+document.write(3 + 3);              //Expression adding numbers
+document.write("<br><br>");         //Line break
