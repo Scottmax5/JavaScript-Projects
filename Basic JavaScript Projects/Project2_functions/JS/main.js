7ec94cb6206d37d34301035d33e576a38e4743d3
Scottmax5 <127995861+Scottmax5@users.noreply.github.com>
2023-04-17 11:17:46 -0700

    Prjoect2_functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+//a function that uses the += operator
+
+function myFunction() {
+	var sentence = "I am learning";
+	sentence += " a lot from this book!";
+	document.getElementById("Concatenate").innerHTML = sentence;
+}
