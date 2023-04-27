a8366c4ac76908bfed2f89a582635ab9dbbbed60
Scottmax5 <127995861+Scottmax5@users.noreply.github.com>
2023-04-26 17:21:08 -0700

    Tic Tac Toe project

---
+++ JavaScript/JavaScript_02.js
@@ -0,0 +1,9 @@
+//Form validation
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
